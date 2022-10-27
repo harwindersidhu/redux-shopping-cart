67cae8b81c6cdd7aa1b4ed5389b8d8fa43875ede
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-10-26 22:10:09 -0600

    Set up starting app with different components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+       {/* <Auth /> */}
+       <Layout />
     </div>
   );
 }
